162e2a1f7d7cee255f05bf35dfdc0c66573edf4d
nyykooo <nicholas.cakla@outlook.com>
2025-05-25 12:10:02 +0100

    first implementation of a socket in the server that accepts a client

---
+++ Makefile
@@ -0,0 +1,36 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: ncampbel <ncampbel@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/03/10 19:46:12 by ncampbel          #+#    #+#              #
+#    Updated: 2025/05/24 17:44:54 by ncampbel         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME= Webserv
+
+CXX= c++
+
+CXXFLAGS= -Wall -Wextra -Werror -g -std=c++98
+
+SRC = main.cpp
+
+OBJ = $(SRC:.cpp=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	$(CXX) $(CXXFLAGS) $(OBJ) -o $(NAME)
+
+clean:
+	rm -f $(OBJ)
+	
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ includes/headers.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   headers.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncampbel <ncampbel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/24 18:00:37 by ncampbel          #+#    #+#             */
+/*   Updated: 2025/05/24 19:06:10 by ncampbel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADERS_HPP
+#define HEADERS_HPP
+
+# include <iostream>
+# include <arpa/inet.h>		// includes inet_ntop(), inet_pton(), etc.
+# include <unistd.h>		// includes close(), read(), write(), etc.
+# include <string>			// includes std::string, std::to_string(), etc.
+# include <cstring>			// includes memset(), memcpy(), etc.
+# include <sys/socket.h>	// includes socket(), bind(), accept(), connect(), etc.
+# include <netdb.h>			// includes getaddrinfo(), getnameinfo(), etc.
+
+#endif
